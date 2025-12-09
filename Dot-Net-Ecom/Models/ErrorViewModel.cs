ed197966ec332e3f3ba369450b5bad8efbc539c0
RIZWAN <morizwan1410@gmail.com>
2025-12-09 17:38:11 +0530

    Add project files.

---
+++ Dot-Net-Ecom/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dot_Net_Ecom.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
