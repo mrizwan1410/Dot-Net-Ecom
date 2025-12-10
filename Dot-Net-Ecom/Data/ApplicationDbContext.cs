5760188865278136886da1fd2a0e2041bb78dc81
RIZWAN <morizwan1410@gmail.com>
2025-12-10 17:12:46 +0530

    Setting DbContext and sqlserver

---
+++ Dot-Net-Ecom/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Dot_Net_Ecom.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dot_Net_Ecom.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Dot-Net-Ecom/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dot_Net_Ecom.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
