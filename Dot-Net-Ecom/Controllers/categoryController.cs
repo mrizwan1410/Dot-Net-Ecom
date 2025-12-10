6586e9c08539b5012c4574164bf6c7d3453f3784
RIZWAN <morizwan1410@gmail.com>
2025-12-10 17:25:07 +0530

    Setting up CategoryController

---
+++ Dot-Net-Ecom/Controllers/categoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dot_Net_Ecom.Controllers
+{
+    public class categoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
