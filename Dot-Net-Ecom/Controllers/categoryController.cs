fb7d209c7efafa88e409be5b20c7cecccffc0235
RIZWAN <morizwan1410@gmail.com>
2025-12-10 19:34:38 +0530

    Get All Categories

--- Dot-Net-Ecom/Controllers/categoryController.cs
+++ Dot-Net-Ecom/Controllers/categoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dot_Net_Ecom.Data;
+using Dot_Net_Ecom.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dot_Net_Ecom.Controllers
 {
-    public class categoryController : Controller
+    public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
