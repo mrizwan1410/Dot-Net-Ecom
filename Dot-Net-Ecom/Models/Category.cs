7b7fa4d6ecd16e52985a0a60fa1f3d432028451c
RIZWAN <morizwan1410@gmail.com>
2025-12-12 00:54:49 +0530

    Create Category

--- Dot-Net-Ecom/Models/Category.cs
+++ Dot-Net-Ecom/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Dot_Net_Ecom.Models
 {
@@ -8,7 +9,10 @@ namespace Dot_Net_Ecom.Models
         public int Id { get; set; }
 
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
