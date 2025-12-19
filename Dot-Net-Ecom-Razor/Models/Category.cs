8e5139576acbd108f6eb4045b5b007f81bce567c
RIZWAN <morizwan1410@gmail.com>
2025-12-19 20:32:52 +0530

    RazorPages setup

---
+++ Dot-Net-Ecom-Razor/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dot_Net_Ecom_Razor.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+
+        [Range(1, 100, ErrorMessage = "Display Order must be in range of 1 and 100")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
---
+++ Dot-Net-Ecom-Razor/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dot_Net_Ecom_Razor.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
