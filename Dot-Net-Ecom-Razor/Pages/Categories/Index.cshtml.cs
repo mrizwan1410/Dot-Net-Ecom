187534a0e71154a01fc2dd102db88a96240ccfdb
RIZWAN <morizwan1410@gmail.com>
2025-12-26 20:09:25 +0530

    first Razor page

---
+++ Dot-Net-Ecom-Razor/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dot_Net_Ecom_Razor.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
