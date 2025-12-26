3e7d3e66451102a2d7c3d10dac2a84c74898ec4c
RIZWAN <morizwan1410@gmail.com>
2025-12-26 20:25:35 +0530

    Razor - Display all Categories

--- Dot-Net-Ecom-Razor/Pages/Categories/Index.cshtml.cs
+++ Dot-Net-Ecom-Razor/Pages/Categories/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using Dot_Net_Ecom_Razor.Data;
+using Dot_Net_Ecom_Razor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,15 @@ namespace Dot_Net_Ecom_Razor.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public List<Category> CategoryList { get; set; }
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
+            CategoryList = _db.Categories.ToList();
         }
     }
 }
