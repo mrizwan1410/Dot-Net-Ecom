e98ce021b5e879791d819ef2e6ffbbaac9ad8b96
RIZWAN <morizwan1410@gmail.com>
2025-12-28 00:28:24 +0530

    Razor - Delete Category

--- Dot-Net-Ecom-Razor/Pages/Categories/Delete.cshtml.cs
+++ Dot-Net-Ecom-Razor/Pages/Categories/Delete.cshtml.cs
@@ -1,12 +1,38 @@
+using Dot_Net_Ecom_Razor.Data;
+using Dot_Net_Ecom_Razor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Dot_Net_Ecom_Razor.Pages.Categories
 {
+    [BindProperties]
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            Category? obj = _db.Categories.Find(Category.Id);
+            if(obj==null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
         }
     }
 }
