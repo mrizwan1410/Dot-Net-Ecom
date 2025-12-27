c16d583d1cb7b8a88166cac9a7033183b4e0141d
RIZWAN <morizwan1410@gmail.com>
2025-12-28 00:19:41 +0530

    Razor - Edit Category

---
+++ Dot-Net-Ecom-Razor/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dot_Net_Ecom_Razor.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Dot-Net-Ecom-Razor/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,38 @@
+using Dot_Net_Ecom_Razor.Data;
+using Dot_Net_Ecom_Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dot_Net_Ecom_Razor.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if(id!=null && id!=0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
+                _db.SaveChanges();
+                //TempData["Success"] = "Category updated successfully";
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
