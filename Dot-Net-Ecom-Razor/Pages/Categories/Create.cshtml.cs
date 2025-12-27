cff312659824872d965af1a149baf80ff08f1006
RIZWAN <morizwan1410@gmail.com>
2025-12-27 22:55:51 +0530

    Razor - Create category UI

---
+++ Dot-Net-Ecom-Razor/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,21 @@
+using Dot_Net_Ecom_Razor.Data;
+using Dot_Net_Ecom_Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dot_Net_Ecom_Razor.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
