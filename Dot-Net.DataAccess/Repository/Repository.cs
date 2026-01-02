e51bf76715822db8096ad7d6e315f54198a2e9ae
RIZWAN <morizwan1410@gmail.com>
2026-01-02 20:47:17 +0530

    Repositoy interface implemetation

--- Dot-Net.DataAccess/Repository/Repository.cs
+++ Dot-Net.DataAccess/Repository/Repository.cs
@@ -1,4 +1,6 @@
-﻿using Dot_Net.DataAccess.Repository.IRepository;
+﻿using Dot_Net.DataAccess.Data;
+using Dot_Net.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,29 +12,40 @@ namespace Dot_Net.DataAccess.Repository
 {
     public class Repository<T> : IRepository<T> where T : class
     {
+        private readonly ApplicationDbContext _db;
+
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>(); // _db.Categories==dbSet
+        }
         public void Add(T entity)
         {
-            throw new NotImplementedException();
+            dbSet.Add(entity);
         }
 
         public T Get(Expression<Func<T, bool>> filter)
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
         }
 
         public IEnumerable<T> GetAll()
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = dbSet;
+            return query.ToList();
         }
 
         public void Remove(T entity)
         {
-            throw new NotImplementedException();
+            dbSet.Remove(entity);
         }
 
         public void RemoveRange(IEnumerable<T> entity)
         {
-            throw new NotImplementedException();
+            dbSet.RemoveRange(entity);
         }
     }
 }
