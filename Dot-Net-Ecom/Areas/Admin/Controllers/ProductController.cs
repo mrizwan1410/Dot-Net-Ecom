7856f44930e5ae5ce68f5513ddf80910a96c034d
RIZWAN <morizwan1410@gmail.com>
2026-01-09 21:26:09 +0530

    implementing ViewModels

--- Dot-Net-Ecom/Areas/Admin/Controllers/ProductController.cs
+++ Dot-Net-Ecom/Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Dot_Net.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Dot_Net.Models.ViewModels;
 
 namespace Dot_Net_Ecom.Areas.Admin.Controllers
 {
@@ -21,29 +22,43 @@ namespace Dot_Net_Ecom.Areas.Admin.Controllers
 
         public IActionResult Create()
         {
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll()
+            // ViewBag.CategoryList = CategoryList;
+            // ViewData["CategoryList"] = CategoryList;
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.Category.GetAll()
+                .Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Product = new Product()
+            };
+
+            return View(productVM);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductVM productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(productVM.Product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index", "Product");
+            }
+            else
+            {
+                productVM.CategoryList = _unitOfWork.Category.GetAll()
                 .Select(u => new SelectListItem
                 {
                     Text = u.Name,
                     Value = u.Id.ToString()
                 });
-            // ViewBag.CategoryList = CategoryList;
-            ViewData["CategoryList"] = CategoryList;
-
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Create(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Add(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product created successfully";
-                return RedirectToAction("Index", "Product");
+                return View(productVM);
             }
-            return View();
+                
         }
 
         public IActionResult Edit(int? id)
---
+++ Dot-Net.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dot_Net.Models.ViewModels
+{
+    public class ProductVM
+    {
+        public Product Product { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+    }
+}
